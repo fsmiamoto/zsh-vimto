65aaceb5a41e6dea4a9b25fb4bebacd0946f0888
Francisco Miamoto <fsmiamoto@gmail.com>
2020-05-16 11:32:48 -0300

    use block cursor

--- zsh-vimto.zsh
+++ zsh-vimto.zsh
@@ -21,14 +21,14 @@ function zle-keymap-select zle-line-init {
 	    # Command mode cursor
 		local vicmd_seq="\e[2 q"
 		# Insert mode cursor
-		local viins_seq="\e[4 q"
+		local viins_seq="\e[2 q"
 	else
 		# In tmux, escape sequences to pass to terminal need to be
 		# surrounded in a DSC sequence and double-escaped:
 		# ESC P tmux; {text} ESC \
 		# <http://linsam.homelinux.com/tmux/tmuxcodes.pdf>
 		local vicmd_seq="\ePtmux;\e\e[2 q\e\\"
-		local viins_seq="\ePtmux;\e\e[4 q\e\\"
+		local viins_seq="\ePtmux;\e\e[2 q\e\\"
 	fi
 
 	# Command mode
